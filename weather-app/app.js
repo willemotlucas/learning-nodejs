24c836f4183f5e572889f731f4748317abe22995
Lucas Willemot <willemotlucas@gmail.com>
2017-02-13 18:34:54 +0100

    start working on weather app in order to fetch weather from Google API

---
+++ weather-app/app.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: 'address',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
+
+request({
+	url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+	json: true
+}, (error, response, body) => {
+	if(error){
+		console.log('Unable to connect to Google servers');
+	} else if(body.status === 'ZERO_RESULTS') {
+		console.log('Unable to find this address');
+	} else if(body.status === 'OK') {
+		console.log(body.results[0].formatted_address);
+		console.log('Latitude', body.results[0].geometry.location.lat);
+		console.log('Longitude', body.results[0].geometry.location.lng);
+	}
+});
