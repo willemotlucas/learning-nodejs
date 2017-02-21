157f178dc70d81a925b83810bcda70269de21219
Lucas Willemot <willemotlucas@gmail.com>
2017-02-21 18:56:54 +0100

    Add tests for async methods

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,8 +1,23 @@
 module.exports.add = (a, b) => a + b;
+
+module.exports.asyncAdd = (a, b, callback) => {
+	setTimeout(() => {
+		callback(a + b);
+	}, 1000);
+}
+
 module.exports.square = (x) => x * x;
+
+module.exports.asyncSquare = (x, callback) => {
+	setTimeout(() => {
+		callback(x * x);
+	}, 1000)
+}
+
 module.exports.setName = (user, fullName) => {
 	var names = fullName.split(' ');
 	user.first_name = names[0];
 	user.last_name = names[1];
 	return user;
 }
+
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -7,11 +7,25 @@ it('should add two numbers', () => {
 	expect(result).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+	utils.asyncAdd(4, 3, (sum) => {
+		expect(sum).toBeA('number').toBe(7);
+		done();
+	});
+});
+
 it('should square a number', () => {
 	var result = utils.square(3);
 
 	expect(result).toBe(9).toBeA('number');
 });
+
+it('should async square a number', (done) => {
+	utils.asyncSquare(4, (res) => {
+		expect(res).toBeA('number').toBe(16);
+		done();
+	});
+})
 
 it('should expect some values', () => {
 	// expect(12).toNotBe(11);
