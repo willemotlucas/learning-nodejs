9039ec43e721b72cf31617572793d85a8700cf23
Lucas Willemot <willemotlucas@gmail.com>
2017-02-21 19:34:05 +0100

    Use describe to better organize tests

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -2,28 +2,36 @@ const request = require('supertest');
 var app = require('./server').app;
 const expect = require('expect');
 
+describe('Server', () => {
 
-it('should return hello world response', (done) => {
-	request(app)
-		.get('/')
-		.expect(404)
-		.expect((res) => {
-			expect(res.body).toInclude({
-				error: 'Page not found'
-			});
-		})
-		.end(done);
+	describe('GET /', () => {
+		it('should return hello world response', (done) => {
+			request(app)
+				.get('/')
+				.expect(404)
+				.expect((res) => {
+					expect(res.body).toInclude({
+						error: 'Page not found'
+					});
+				})
+				.end(done);
+		});
+	});
+
+	describe('GET /users', () => {
+		it('should return an array of users', (done) => {
+			request(app)
+				.get('/users')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body)
+						.toBeA('array')
+						.toInclude({name: 'Marie Daguin', age: 22, location: 'Paris, France'})
+						.toInclude({name: 'Lucas Willemot', age: 22, location: 'Blois, France'});
+				})
+				.end(done);
+		});
+	});
+	
 });
 
-it('should return an array of users', (done) => {
-	request(app)
-		.get('/users')
-		.expect(200)
-		.expect((res) => {
-			expect(res.body)
-				.toBeA('array')
-				.toInclude({name: 'Marie Daguin', age: 22, location: 'Paris, France'})
-				.toInclude({name: 'Lucas Willemot', age: 22, location: 'Blois, France'});
-		})
-		.end(done);
-});
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,50 +1,47 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-	var result = utils.add(33, 11);
+describe('Utils', () => {
 
-	expect(result).toBe(44).toBeA('number');
-});
+	describe('#add', () => {
+		it('should add two numbers', () => {
+			var result = utils.add(33, 11);
 
-it('should async add two numbers', (done) => {
-	utils.asyncAdd(4, 3, (sum) => {
-		expect(sum).toBeA('number').toBe(7);
-		done();
+			expect(result).toBe(44).toBeA('number');
+		});
+
+		it('should async add two numbers', (done) => {
+			utils.asyncAdd(4, 3, (sum) => {
+				expect(sum).toBeA('number').toBe(7);
+				done();
+			});
+		});
 	});
-});
 
-it('should square a number', () => {
-	var result = utils.square(3);
+	describe('#square', () => {
+		it('should square a number', () => {
+			var result = utils.square(3);
 
-	expect(result).toBe(9).toBeA('number');
-});
+			expect(result).toBe(9).toBeA('number');
+		});
 
-it('should async square a number', (done) => {
-	utils.asyncSquare(4, (res) => {
-		expect(res).toBeA('number').toBe(16);
-		done();
+		it('should async square a number', (done) => {
+			utils.asyncSquare(4, (res) => {
+				expect(res).toBeA('number').toBe(16);
+				done();
+			});
+		});
 	});
-})
 
-it('should expect some values', () => {
-	// expect(12).toNotBe(11);
-	// expect({name: 'Lucas'}).toEqual({name: 'Lucas'});
-	// expect([2, 3, 4]).toExclude(5);
-	// expect({
-	// 	name: 'Lucas',
-	// 	age: 22,
-	// 	location: 'Blois'
-	// }).toInclude({
-	// 	age: 22
-	// });
-});
+	describe('#setName', () => {
+		it('should split full name of a user', () => {
+			var user = utils.setName({
+				age: 22,
+				location: 'Blois, France'
+			}, 'Lucas Willemot');
 
-it('should split full name of a user', () => {
-	var user = utils.setName({
-		age: 22,
-		location: 'Blois, France'
-	}, 'Lucas Willemot');
+			expect(user).toBeA('object').toInclude({first_name: 'Lucas', last_name: 'Willemot'});
+		});
+	});
 
-	expect(user).toBeA('object').toInclude({first_name: 'Lucas', last_name: 'Willemot'});
 });
