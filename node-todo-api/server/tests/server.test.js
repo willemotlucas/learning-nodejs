8e26200c560140a9345572c4a32f913e32f3a47e
Lucas Willemot <willemotlucas@gmail.com>
2017-02-28 21:20:12 +0100

    Add GET /todos route and associated tests

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,14 +4,24 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [{
+		text: 'First test todo',
+	}, {
+		text: 'Second test todo'
+	}
+];
+
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
 
 	it('should create a new todo', (done) => {
 		var text = 'Test todo text';
+		var _id = null;
 
 		request(app)
 			.post('/todos')
@@ -19,13 +29,14 @@ describe('POST /todos', () => {
 			.expect(200)
 			.expect((res) => {
 				expect(res.body.text).toBe(text);
+				_id = res.body._id;
 			})
 			.end((err, res) => {
 				if(err) {
 					return done(err);
 				}
 
-				Todo.find().then((todos) => {
+				Todo.find({_id}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -44,10 +55,25 @@ describe('POST /todos', () => {
 				}
 
 				Todo.find().then((todos) => {
-					expect(todos.length).toBe(0);
+					expect(todos.length).toBe(2);
 					done();
 				}).catch((err) => done(err));
 			});
+	});
+
+});
+
+describe('GET /posts', () => {
+
+	it('should return all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.send()
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
 	});
 
 });
