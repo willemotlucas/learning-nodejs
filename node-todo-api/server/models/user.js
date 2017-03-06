49fee304909a6c00abdd8d14299791b95a0517a8
Lucas Willemot <willemotlucas@gmail.com>
2017-03-06 19:10:13 +0100

    Hash password before saving user in database

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
 	email: {
@@ -73,6 +74,24 @@ UserSchema.statics.findByToken = function(token) {
 		'tokens.access': 'auth'
 	});
 }
+
+// Define pre operational methods
+
+UserSchema.pre('save', function(next) {
+	var user = this;
+
+	if(user.isModified('password')) {
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
+
 
 // Create User model from user schema
 
